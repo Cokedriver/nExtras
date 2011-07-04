29c4ad5dfc59baecc10b1db2431b8c0e54868651
Cokedriver <K_M_A_2004@hotmail.com>
2011-07-04 15:07:54 -0700

    Made changes to chat.lua so that if you make a any new window they have the !Beautycase border.

--- nExtras/modules/chat.lua
+++ nExtras/modules/chat.lua
@@ -1,16 +1,21 @@
 if nExtras.chat.enable ~= true then return end
 
-if (IsAddOnLoaded("nChat")) then
-	do 
-		ChatFrame1:CreateBeautyBorder(12)
-		ChatFrame1:SetBeautyBorderPadding( 5, 5, 5, 5, 5, 8, 5, 8)
-		ChatFrame2:CreateBeautyBorder(12)
-		ChatFrame2:SetBeautyBorderPadding(5, 29, 5, 29, 5, 8, 5, 8)
-		ChatFrame3:CreateBeautyBorder(12)
-		ChatFrame3:SetBeautyBorderPadding(5, 5, 5, 5, 5, 8, 5, 8)
-		ChatFrame4:CreateBeautyBorder(12)
-		ChatFrame4:SetBeautyBorderPadding(5, 5, 5, 5, 5, 8, 5, 8)
-		ChatFrame5:CreateBeautyBorder(12)
-		ChatFrame5:SetBeautyBorderPadding(5, 5, 5, 5, 5, 8, 5, 8)
+local _G = _G
+
+if (IsAddOnLoaded('!Beautycase')) then
+	do	
+		for i = 1, NUM_CHAT_WINDOWS do
+			local cf = _G['ChatFrame'..i]
+			if cf then
+				cf:CreateBeautyBorder(12)
+				cf:SetBeautyBorderPadding( 5, 5, 5, 5, 5, 8, 5, 8)
+			end
+		end
+		
+		local ct = _G['ChatFrame2']
+		if ct then
+			ct:CreateBeautyBorder(12)
+			ct:SetBeautyBorderPadding(5, 29, 5, 29, 5, 8, 5, 8)
+		end
 	end
 end
